0315c68b79c08b654d431f768300bcc3128c1403
Sebastian Vollnhals <sebastian@vollnhals.info>
2014-05-28 17:30:48 +0200

    +gender, frontent styling

--- dist/assets/js/nebeneinkuenfte.js
+++ dist/assets/js/nebeneinkuenfte.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 	
+	/* make tables sortable */
 	$('article table').each(function(idx,e){
 		$(e).tablesorter({sortInitialOrder: "desc"});
+	});
+
+	/* set content min height */
+	$('#main').css("min-height", $(window).innerHeight()-($('header').outerHeight()+$('footer').outerHeight()));
+	$(window).resize(function(){
+		$('#main').css("min-height", $(window).innerHeight()-($('header').outerHeight()+$('footer').outerHeight()));
+	});
+	
+	/* make tabs linkable */
+	if (document.location.hash) $('.nav-tabs a[href=#'+document.location.hash.replace(/[^a-z]/g,'')+']').tab('show') ;
+	$('.nav-tabs a').on('shown.bs.tab', function(e) {
+		$(this).blur();
+		window.location.hash = e.target.hash.replace(/^#/,'/');
 	});
 	
 });
